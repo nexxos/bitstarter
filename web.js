3e356c0fdbb857d59befb84cc5eb5d871e1e851a
Oliver Schafeld <info@nexxos.de>
2013-07-09 13:22:26 +0000

    web.js reads text from index.html

--- web.js
+++ web.js
@@ -1,12 +1,30 @@
-var express = require('express');
+// var express = require('express');
+// var app = express.createServer(express.logger());
 
-var app = express.createServer(express.logger());
+var fs = require('fs');
 
+/*
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send('Hello World!');
+});
+*/
+
+
+
+fs.readFile('index.html', 'utf8', function (err, data) {
+  if (err) throw err;
+  // out_text = buf.write(data, 0);
+  //buf = new Buffer(256);
+  //len = buf.write(data, 0);
+  // out_text = data.toString;
+  console.log(data);
+  //output_text = buf.toString('utf8', 0, len); 
+  //console.log(output_text);
 });
 
+/*
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+*/
