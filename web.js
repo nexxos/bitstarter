c2fe7eddd8e5902fcb7b1cd01eeb8d52f35758eb
Oliver Schafeld <info@nexxos.de>
2013-07-09 13:30:10 +0000

    web.js outputs as server

--- web.js
+++ web.js
@@ -1,16 +1,18 @@
-// var express = require('express');
-// var app = express.createServer(express.logger());
+var express = require('express');
+var app = express.createServer(express.logger());
 
 var fs = require('fs');
 
-/*
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  // response.send('Hello World!');
+	  fs.readFile('index.html', 'utf8', function (err, data) {
+	  if (err) throw err;
+	  response.send(data);
+	});
 });
-*/
 
-
-
+/*
 fs.readFile('index.html', 'utf8', function (err, data) {
   if (err) throw err;
   // out_text = buf.write(data, 0);
@@ -22,9 +24,11 @@ fs.readFile('index.html', 'utf8', function (err, data) {
   //console.log(output_text);
 });
 
-/*
+*/
+
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-*/
